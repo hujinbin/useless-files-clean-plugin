ec9fd4b2fb14ca21bf83872cfc5a60a31e0c0e92
hujinbin <437044048@qq.com>
2023-10-25 10:45:45 +0800

    feat: 忽略文件夹功能和参数配置

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,13 @@ const path = require('path')
 const shelljs = require('shelljs')
 
 class UselessFilesCleanPlugin {
+  public rootPath: string = ''
   private opts: any = {}
   constructor (options: any = {}) {
     this.opts = options
-    console.log(this.opts)
   }
   apply (compiler:any) {
     compiler.plugin('after-emit', (compilation:any, done:any)=> {
-      console.log(22222222)
       this.findUnusedFiles(compilation, this.opts)
       done()
     })
@@ -39,45 +38,41 @@ class UselessFilesCleanPlugin {
  * 获取项目目录所有的文件
  */
   getAllFiles (pattern:any) {
-    console.log("getAllFiles====")
-    console.log(pattern)
     return new Promise((resolve, reject) => {
-      const files =  globSync(pattern)
+      const files =  globSync(pattern,{
+        nodir: true,
+      })
       const out = files.map((item:any) => path.resolve(item))
       resolve(out)
     })
   }
-  // 过滤忽略的文件
+  // 过滤忽略的文件夹
   dealExclude (path:any, unusedList:any) {
-    console.log(path,unusedList)
-    const file = fs.readFileSync(path, 'utf-8')
-    const files = JSON.parse(file) || []
-    const result = unusedList.filter((unused:any) => {
-      return !files.some((item:any) => ~unused.indexOf(item))
-    })
+    let index = this.rootPath.length;
+    const result = unusedList.filter((unused:any) => (unused.indexOf(`\\${path}\\`) < index  && unused.indexOf(`/${path}/`) < index))
     return result
   }
   // 查找需要删除的冗余文件
   async findUnusedFiles (compilation:any, config = {root:String,clean:Boolean,output:String,exclude:Array,ignoreFile:Array}) {
     const { root  = './src', clean = false, output = './unused-files.json', exclude = [],ignoreFile = [] } = config
-    console.log(root, clean, output, ignoreFile)
     const pattern = root + '/**/*'
+    this.rootPath = path.join(process.cwd(), root);
     try {
       const allChunks:any = await this.getDependFiles(compilation)
       const allFiles:any = await this.getAllFiles(pattern)
       let unUsed = allFiles
         .filter((item:any) => !~allChunks.indexOf(item))
-      if (exclude.length > 0) {
-        unUsed = this.dealExclude(exclude, unUsed)
+      if (Array.isArray(exclude) && exclude.length > 0) {
+        exclude.forEach((path:string)=>{
+          unUsed = this.dealExclude(path, unUsed)
+        })
       }
-      console.log(unUsed)
       // 过滤指定后缀名称
       if(Array.isArray(ignoreFile) && ignoreFile.length >0){
         ignoreFile.forEach((file:string)=>{
           unUsed = unUsed.filter((path:any)=> !(path.indexOf(file) > -1 && path.indexOf(file) === (path.length - file.length)))
         })
       }
-      console.log(unUsed)
       if (typeof output === 'string') {
         fs.writeFileSync(output, JSON.stringify(unUsed, null, 4))
       } else if (typeof output === 'function') {
